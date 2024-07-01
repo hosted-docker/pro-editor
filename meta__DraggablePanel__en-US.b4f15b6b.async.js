85a99706d7a43b94957e64d1b92909f2d3a86b6c
beeant0512 <huangxb0512@gmail.com>
2024-07-01 06:02:05 +0000

    Deploying to gh-pages from @ hosted-docker/pro-editor@dbc49e7b275238deb3580018ba203aed2356663d 🚀

---
+++ meta__DraggablePanel__en-US.b4f15b6b.async.js
@@ -0,0 +1,92 @@
+"use strict";(self.webpackChunk_ant_design_pro_editor=self.webpackChunk_ant_design_pro_editor||[]).push([[6833],{93061:function(s,a,e){e.r(a),e.d(a,{demos:function(){return I}});var m=e(90228),o=e.n(m),c=e(87999),u=e.n(c),d=e(75271),I={"draggablepanel-demo-basic":{component:d.memo(d.lazy(function(){return e.e(7102).then(e.bind(e,59302))})),asset:{type:"BLOCK",id:"draggablepanel-demo-basic",refAtomIds:["DraggablePanel"],dependencies:{"index.tsx":{type:"FILE",value:e(34526).Z},"@ant-design/pro-editor":{type:"NPM",value:"1.1.2"}},entry:"index.tsx",description:"By default, the panel on the right is fixed and can be dragged left and right",title:"Fixed Panel on the Right"},context:{"@ant-design/pro-editor":e(53756)},renderOpts:{compile:function(){var l=u()(o()().mark(function i(){var t,p=arguments;return o()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(8865).then(e.bind(e,58865));case 2:return n.abrupt("return",(t=n.sent).default.apply(t,p));case 3:case"end":return n.stop()}},i)}));function r(){return l.apply(this,arguments)}return r}()}},"draggablepanel-demo-left":{component:d.memo(d.lazy(function(){return e.e(7102).then(e.bind(e,83481))})),asset:{type:"BLOCK",id:"draggablepanel-demo-left",refAtomIds:["DraggablePanel"],dependencies:{"index.tsx":{type:"FILE",value:e(97872).Z},"@ant-design/pro-editor":{type:"NPM",value:"1.1.2"},"react-layout-kit":{type:"NPM",value:"1.9.0"}},entry:"index.tsx",description:"The panel on the left is fixed and can be dragged left and right",title:"Fixed Panel on the Left"},context:{"@ant-design/pro-editor":e(53756),"react-layout-kit":e(6020)},renderOpts:{compile:function(){var l=u()(o()().mark(function i(){var t,p=arguments;return o()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(8865).then(e.bind(e,58865));case 2:return n.abrupt("return",(t=n.sent).default.apply(t,p));case 3:case"end":return n.stop()}},i)}));function r(){return l.apply(this,arguments)}return r}()}},"draggablepanel-demo-bottom":{component:d.memo(d.lazy(function(){return e.e(7102).then(e.bind(e,82001))})),asset:{type:"BLOCK",id:"draggablepanel-demo-bottom",refAtomIds:["DraggablePanel"],dependencies:{"index.tsx":{type:"FILE",value:e(18058).Z},"@ant-design/pro-editor":{type:"NPM",value:"1.1.2"},"react-layout-kit":{type:"NPM",value:"1.9.0"}},entry:"index.tsx",description:"The panel at the bottom is fixed and can be dragged up and down",title:"Fixed Panel at the Bottom"},context:{"@ant-design/pro-editor":e(53756),"react-layout-kit":e(6020)},renderOpts:{compile:function(){var l=u()(o()().mark(function i(){var t,p=arguments;return o()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(8865).then(e.bind(e,58865));case 2:return n.abrupt("return",(t=n.sent).default.apply(t,p));case 3:case"end":return n.stop()}},i)}));function r(){return l.apply(this,arguments)}return r}()}},"draggablepanel-demo-top":{component:d.memo(d.lazy(function(){return e.e(7102).then(e.bind(e,37177))})),asset:{type:"BLOCK",id:"draggablepanel-demo-top",refAtomIds:["DraggablePanel"],dependencies:{"index.tsx":{type:"FILE",value:e(1557).Z},"@ant-design/pro-editor":{type:"NPM",value:"1.1.2"},"react-layout-kit":{type:"NPM",value:"1.9.0"}},entry:"index.tsx",description:"The panel at the top is fixed and can be dragged up and down",title:"Fixed Panel at the Top"},context:{"@ant-design/pro-editor":e(53756),"react-layout-kit":e(6020)},renderOpts:{compile:function(){var l=u()(o()().mark(function i(){var t,p=arguments;return o()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(8865).then(e.bind(e,58865));case 2:return n.abrupt("return",(t=n.sent).default.apply(t,p));case 3:case"end":return n.stop()}},i)}));function r(){return l.apply(this,arguments)}return r}()}},"draggablepanel-demo-float":{component:d.memo(d.lazy(function(){return e.e(7102).then(e.bind(e,80128))})),asset:{type:"BLOCK",id:"draggablepanel-demo-float",refAtomIds:["DraggablePanel"],dependencies:{"index.tsx":{type:"FILE",value:e(6385).Z},"@ant-design/pro-editor":{type:"NPM",value:"1.1.2"}},entry:"index.tsx",description:"Set mode to float",title:"Floating Draggable Panel"},context:{"@ant-design/pro-editor":e(53756)},renderOpts:{compile:function(){var l=u()(o()().mark(function i(){var t,p=arguments;return o()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(8865).then(e.bind(e,58865));case 2:return n.abrupt("return",(t=n.sent).default.apply(t,p));case 3:case"end":return n.stop()}},i)}));function r(){return l.apply(this,arguments)}return r}()}},"draggablepanel-demo-controlfloat":{component:d.memo(d.lazy(function(){return e.e(7102).then(e.bind(e,37201))})),asset:{type:"BLOCK",id:"draggablepanel-demo-controlfloat",refAtomIds:["DraggablePanel"],dependencies:{"index.tsx":{type:"FILE",value:e(26871).Z},"@ant-design/pro-editor":{type:"NPM",value:"1.1.2"},ahooks:{type:"NPM",value:"3.7.8"}},entry:"index.tsx",description:"The position of the floating panel is controlled",title:"Floating Controlled Mode"},context:{"@ant-design/pro-editor":e(53756),ahooks:e(42109)},renderOpts:{compile:function(){var l=u()(o()().mark(function i(){var t,p=arguments;return o()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,e.e(8865).then(e.bind(e,58865));case 2:return n.abrupt("return",(t=n.sent).default.apply(t,p));case 3:case"end":return n.stop()}},i)}));function r(){return l.apply(this,arguments)}return r}()}}}},32650:function(s,a,e){e.r(a),e.d(a,{texts:function(){return m}});const m=[{value:"Used for panels that need to be stretched or dragged and moved.",paraId:0,tocIndex:0},{value:"Property",paraId:1,tocIndex:8},{value:"Type",paraId:1,tocIndex:8},{value:"Default",paraId:1,tocIndex:8},{value:"Description",paraId:1,tocIndex:8},{value:"mode",paraId:1,tocIndex:8},{value:"'fixed' | 'float'",paraId:1,tocIndex:8},{value:"'fixed'",paraId:1,tocIndex:8},{value:"The position mode of the panel, using fixed mode or floating window",paraId:1,tocIndex:8},{value:"placement",paraId:1,tocIndex:8},{value:"'right' | 'left' | 'top' | 'bottom'",paraId:1,tocIndex:8},{value:"'right'",paraId:1,tocIndex:8},{value:"The orientation of the panel in fixed mode, default placement is on the right",paraId:1,tocIndex:8},{value:"minWidth",paraId:1,tocIndex:8},{value:"number",paraId:1,tocIndex:8},{value:"Minimum width of the panel",paraId:1,tocIndex:8},{value:"minHeight",paraId:1,tocIndex:8},{value:"number",paraId:1,tocIndex:8},{value:"Minimum height of the panel",paraId:1,tocIndex:8},{value:"maxWidth",paraId:1,tocIndex:8},{value:"number",paraId:1,tocIndex:8},{value:"Maximum width of the panel",paraId:1,tocIndex:8},{value:"maxHeight",paraId:1,tocIndex:8},{value:"number",paraId:1,tocIndex:8},{value:"Maximum height of the panel",paraId:1,tocIndex:8},{value:"resize",paraId:1,tocIndex:8},{value:"RndProps['enableResizing']",paraId:1,tocIndex:8},{value:"Control the resizable area",paraId:1,tocIndex:8},{value:"size",paraId:1,tocIndex:8},{value:"Partial<Size>",paraId:1,tocIndex:8},{value:"Panel size",paraId:1,tocIndex:8},{value:"onSizeChange",paraId:1,tocIndex:8},{value:"(delta: NumberSize, size?: Size) => void",paraId:1,tocIndex:8},{value:"Callback for panel size change",paraId:1,tocIndex:8},{value:"onSizeDragging",paraId:1,tocIndex:8},{value:"(delta: NumberSize, size?: Size) => void",paraId:1,tocIndex:8},{value:"Callback during panel size dragging",paraId:1,tocIndex:8},{value:"expandable",paraId:1,tocIndex:8},{value:"boolean",paraId:1,tocIndex:8},{value:"true",paraId:1,tocIndex:8},{value:"Whether it can be expanded",paraId:1,tocIndex:8},{value:"isExpand",paraId:1,tocIndex:8},{value:"boolean",paraId:1,tocIndex:8},{value:"Whether it is currently in the expanded state",paraId:1,tocIndex:8},{value:"onExpandChange",paraId:1,tocIndex:8},{value:"(expand: boolean) => void",paraId:1,tocIndex:8},{value:"Callback for expand state change",paraId:1,tocIndex:8},{value:"position",paraId:1,tocIndex:8},{value:"RndProps['position']",paraId:1,tocIndex:8},{value:"Panel position in controlled mode",paraId:1,tocIndex:8},{value:"defaultSize",paraId:1,tocIndex:8},{value:"Partial<Size>",paraId:1,tocIndex:8},{value:"Default panel size",paraId:1,tocIndex:8},{value:"defaultPosition",paraId:1,tocIndex:8},{value:"RndProps['position']",paraId:1,tocIndex:8},{value:"[100, 100]",paraId:1,tocIndex:8},{value:"Default panel position, valid in floating mode",paraId:1,tocIndex:8},{value:"onPositionChange",paraId:1,tocIndex:8},{value:"(position: RndProps['position']) => void",paraId:1,tocIndex:8},{value:"Callback for panel position change",paraId:1,tocIndex:8},{value:"children",paraId:1,tocIndex:8},{value:"ReactNode",paraId:1,tocIndex:8},{value:"Panel content",paraId:1,tocIndex:8}]},34526:function(s,a){a.Z=`import { DraggablePanel } from '@ant-design/pro-editor';
+
+export default () => (
+  <div
+    style={{
+      height: 300,
+      display: 'flex',
+      overflow: 'hidden',
+      maxWidth: '100%',
+    }}
+  >
+    <div style={{ flex: 1, padding: 12 }}>Content</div>
+    <DraggablePanel style={{ padding: 12 }} maxWidth={600}>
+      Right Panel
+    </DraggablePanel>
+  </div>
+);
+`},18058:function(s,a){a.Z=`import { DraggablePanel } from '@ant-design/pro-editor';
+import { Flexbox } from 'react-layout-kit';
+
+export default () => (
+  <Flexbox style={{ height: 300 }}>
+    <div style={{ flex: 1, padding: 12 }}>Content</div>
+    <DraggablePanel placement="bottom" maxHeight={250} style={{ width: '100%', padding: 12 }}>
+      Bottom Panel
+    </DraggablePanel>
+  </Flexbox>
+);
+`},26871:function(s,a){a.Z=`import type { Position } from '@ant-design/pro-editor';
+import { DraggablePanel } from '@ant-design/pro-editor';
+import { useLocalStorageState } from 'ahooks';
+
+export default () => {
+  const [position, setPos] = useLocalStorageState<Position>('demo-pos');
+
+  return (
+    <div
+      style={{
+        height: 500,
+        display: 'flex',
+      }}
+    >
+      <DraggablePanel
+        position={position}
+        onPositionChange={setPos}
+        mode="float"
+        style={{ padding: 12 }}
+      >
+        Float Draggable Panel
+      </DraggablePanel>
+    </div>
+  );
+};
+`},6385:function(s,a){a.Z=`import { DraggablePanel } from '@ant-design/pro-editor';
+
+export default () => {
+  return (
+    <div
+      style={{
+        height: 500,
+        display: 'flex',
+      }}
+    >
+      <DraggablePanel style={{ padding: 12 }} mode="float">
+        Float Draggable Panel
+      </DraggablePanel>
+    </div>
+  );
+};
+`},97872:function(s,a){a.Z=`import { DraggablePanel } from '@ant-design/pro-editor';
+import { Flexbox } from 'react-layout-kit';
+
+export default () => (
+  <Flexbox horizontal style={{ height: 300 }}>
+    <DraggablePanel placement="left" maxWidth={800} style={{ width: '100%', padding: 12 }}>
+      Left Panel
+    </DraggablePanel>
+    <div style={{ flex: 1, padding: 12 }}>Content</div>
+  </Flexbox>
+);
+`},1557:function(s,a){a.Z=`import { DraggablePanel } from '@ant-design/pro-editor';
+import { Flexbox } from 'react-layout-kit';
+
+export default () => (
+  <Flexbox style={{ height: 300 }}>
+    <DraggablePanel placement="top" maxHeight={250} style={{ width: '100%', padding: 12 }}>
+      Top Panel
+    </DraggablePanel>
+    <div style={{ flex: 1, padding: 12 }}>Content</div>
+  </Flexbox>
+);
+`},6020:function(s,a,e){e.r(a),e.d(a,{Center:function(){return m.Z},Flexbox:function(){return o.D}});var m=e(55471),o=e(90142)}}]);
